36857127919da5e714649f9102089196e534fb8c
KnucklesSG1 <knucklessg1@gmail.com>
2026-02-19 00:32:36 -0600

    Bump version: 0.1.13 → 0.1.14

--- archivebox_api/archivebox_mcp.py
+++ archivebox_api/archivebox_mcp.py
@@ -26,7 +26,7 @@ from archivebox_api.archivebox_api import Api
 from archivebox_api.utils import to_boolean, to_integer
 from archivebox_api.middlewares import UserTokenMiddleware, JWTClaimsLoggingMiddleware
 
-__version__ = "0.1.13"
+__version__ = "0.1.14"
 
 logger = get_logger(name="TokenMiddleware")
 logger.setLevel(logging.DEBUG)
